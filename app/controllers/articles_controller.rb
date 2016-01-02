08b314dfb6bcaaec8a1c48e8aa026b8212eee07f
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-03 02:18:31 +0900

    Implement show action in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.published.page(params[:page])
   end
 
   def show
+    @article.increment!(:views)
   end
 
   def new
@@ -19,5 +21,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
