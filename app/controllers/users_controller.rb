053500c06aff306416bbca521821dd98cea8d605
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-03 01:29:15 +0900

    Implement create action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def create
+    @user = User.create(user_params)
+  rescue => ex
+    logger.debug ex.message
+    logger.debug ex.backtrace.join("\n")
+    head :bad_request
+  end
+
+  private
+  def user_params
+    params.require(:users).permit(:email, :password)
   end
 end
