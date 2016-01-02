e3d64b65b17d9f16c18d517e98ccd7eee3c4f9a7
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-03 01:54:33 +0900

    Create item model

---
+++ db/migrate/20160102165118_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string     :type,     null: :false
+      t.string     :title
+      t.text       :description
+      t.text       :image_url
+      t.text       :thumbnail_url
+      t.string     :source
+      t.text       :source_url
+      t.string     :snippet
+      t.references :articles, index: true
+
+      t.timestamps null: false
+    end
+    add_index :items, :type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102164522) do
+ActiveRecord::Schema.define(version: 20160102165118) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",         limit: 255,               null: false
@@ -34,6 +34,23 @@ ActiveRecord::Schema.define(version: 20160102164522) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "type",          limit: 255
+    t.string   "title",         limit: 255
+    t.text     "description",   limit: 65535
+    t.text     "image_url",     limit: 65535
+    t.text     "thumbnail_url", limit: 65535
+    t.string   "source",        limit: 255
+    t.text     "source_url",    limit: 65535
+    t.string   "snippet",       limit: 255
+    t.integer  "articles_id",   limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "items", ["articles_id"], name: "index_items_on_articles_id", using: :btree
+  add_index "items", ["type"], name: "index_items_on_type", using: :btree
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", limit: 4,     null: false
