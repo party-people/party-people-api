47df001c1b6509b22373e23fa0b649ae4c90e1c1
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-04 20:53:14 +0900

    Implement actions in article/item controller

--- app/controllers/api/v1/articles/items_controller.rb
+++ app/controllers/api/v1/articles/items_controller.rb
@@ -1,18 +1,32 @@
 class Api::V1::Articles::ItemsController < ApplicationController
   before_action :doorkeeper_authorize!
-  before_action :set_article
+  before_action :set_article, only: [:show]
+  before_action :set_item, only: [:update, :destroy]
 
   def create
+    @article.items.build(item_params)
   end
 
   def update
+    @item.update(item_params)
   end
 
   def destroy
+    @item.destroy
   end
 
   private
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :type, :title, :description, :image_url, :thumbnail_url, :source, :source_url, :snippet
+    )
   end
 end
