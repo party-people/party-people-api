5c8a8892205c32912449c2a117d478297fb3417b
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-04 20:20:40 +0900

    Define restful actions in item controller

--- app/controllers/api/v1/articles/items_controller.rb
+++ app/controllers/api/v1/articles/items_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::Articles::ItemsController < ApplicationController
+  before_action :doorkeeper_authorize!
+  before_action :set_article
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :articles
       resource  :current_user, path: 'me', controller: :current_user, only: [:show]
+      resources :articles do
+        resources :items, only: [:create, :update, :destroy]
+      end
     end
   end
 end
