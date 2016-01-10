59fc68e61bc0d20d823c914f74cab1643a265c71
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2016-01-10 12:26:49 +0900

    Add avatar column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110031348) do
+ActiveRecord::Schema.define(version: 20160110032426) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255,               null: false
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20160110031348) do
     t.string   "email",            limit: 255, null: false
     t.string   "crypted_password", limit: 255
     t.string   "salt",             limit: 255
+    t.string   "avatar",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
